53334846e44192ed524968399c1f95ab0e3a6bd2
nahmanmate <192176108+nahmanmate@users.noreply.github.com>
2025-11-17 01:14:01 +1100

    added treesitter support for languages - javascript - typescript - go

--- init.lua
+++ init.lua
@@ -691,6 +691,9 @@ require('lazy').setup({
         --
         --Astro Language Server
         astro = {},
+        --
+        --Go Language Server
+        gopls = {},
         -- TailwindCSS
         tailwindcss = {},
 
@@ -954,7 +957,22 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'javascript',
+        'typescript',
+        'go',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
