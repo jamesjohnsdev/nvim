d441e3dc0149a29cc9f36e48b106400e55e6370f
nahmanmate <192176108+nahmanmate@users.noreply.github.com>
2025-11-23 12:31:23 +1100

    added snippets

--- lua/kickstart/plugins/blink.lua
+++ lua/kickstart/plugins/blink.lua
@@ -21,12 +21,12 @@ return {
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
         opts = {},
       },
@@ -95,9 +95,8 @@ return {
 
       -- Shows a signature help window while you type arguments for a function
       signature = { enabled = true },
-    }
-  }
+    },
+  },
 }
-
 
 -- vim: ts=2 sts=2 sw=2 et
