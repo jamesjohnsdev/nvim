d30c65caeac28cc88adfc51d74a23c771dd1207e
nahmanmate <192176108+nahmanmate@users.noreply.github.com>
2026-02-06 14:00:37 +1100

    fix: make cmp blink config comply with update

--- lua/kickstart/plugins/blink.lua
+++ lua/kickstart/plugins/blink.lua
@@ -96,18 +96,6 @@ return {
 
       -- Shows a signature help window while you type arguments for a function
       signature = { enabled = true },
-
-      providers = {
-        css_vars = {
-          name = 'css-vars',
-          module = 'css-vars.blink',
-          opts = {
-            -- WARNING: The search is not optimized to look for variables in JS files.
-            -- If you change the search_extensions you might get false positives and weird completion results.
-            search_extensions = { '.js', '.ts', '.jsx', '.tsx' },
-          },
-        },
-      },
     },
   },
 }
